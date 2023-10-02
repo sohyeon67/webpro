58c52f27cbfdffc8ae56f12777689bc88c0434b5
sohyeon67 <77038550+sohyeon67@users.noreply.github.com>
2023-10-02 17:21:25 +0900

    Add files via upload

--- jqpro/src/kr/or/ddit/prod/controller/ProdDetail.java
+++ jqpro/src/kr/or/ddit/prod/controller/ProdDetail.java
@@ -7,35 +7,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ProdDetail
- */
+import kr.or.ddit.prod.service.IProdService;
+import kr.or.ddit.prod.service.ProdServiceImpl;
+import kr.or.ddit.prod.vo.ProdVO;
+
 @WebServlet("/ProdDetail.do")
 public class ProdDetail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public ProdDetail() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		// 클라이언트 요청시 전송데이터 받기 - pid
+		String prodId = request.getParameter("pid");
+		
+		// service객체 얻기
+		IProdService service = ProdServiceImpl.getService();
+		
+		// service메소드 호출 - vo
+		ProdVO vo = service.selectByIdDetail(prodId);
+		
+		// vo 결과값을 request에 저장
+		request.setAttribute("result", vo);
+		
+		// jsp view페이지 설정 - // forward
+		request.getRequestDispatcher("0927/prodDetail.jsp").forward(request, response);
+		
 	}
 
 }
